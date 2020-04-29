ee5a5d2dd5ba5f4847169af6cf4b2bc857ab24ec
Danielle Kogan <kogandanielle@gmail.com>
2020-04-29 13:24:45 -0400

    oof

--- mergeMeetingTimes.js
+++ mergeMeetingTimes.js
@@ -1,14 +1,18 @@
-const mergeMeetings = (meetingsArr) => {
-  let mergedMeetings = [meetingsArr[0]];
-  for (let i = 1; i < meetingsArr.length; i++) {
+const mergeMeetings = (meetings) => {
+
+  let sortedMeetings = meetings.sort((a,b) => a.st - b.st)
+  let mergedMeetings = [sortedMeetings[0]];
+  //Go through meetings compare each w/ last merged
+  for(let i = 1; i < sortedMeetings.length; i++) {
+    //if the end time of meeting 2 is less than or equal to end time of meeting 1, merge
     let lastMerged = mergedMeetings[mergedMeetings.length - 1]
-    if(lastMerged.end < meetingsArr[i].st) {
-      mergedMeetings.push(meetingsArr[i])
-    } else {
-     mergedMeetings[mergedMeetings.length-1] = {st: lastMerged.st, end: meetingsArr[i].end}
-    }
+    let currMeeting= sortedMeetings[i]
+    if(currMeeting.st <= lastMerged.end) {
+      lastMerged.end = Math.max(lastMerged.end, currMeeting.end)
+    } else mergedMeetings.push(currMeeting)
   }
   return mergedMeetings;
 }
 
-module.exports = mergeMeetings;
+module.exports = mergeMeetings
+
